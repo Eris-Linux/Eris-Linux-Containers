b41f43d8ccf73ca7b334cf33c4af682ad30e3074
Christophe Blaess <christophe.blaess@logilin.fr>
2026-06-06 18:36:03 +0200

    Add the `10-hello-rust-minimal` example directory.

---
+++ 10-hello-rust-minimal/src/main.rs
@@ -0,0 +1,30 @@
+use axum::{Router, response::Html, routing::get};
+use std::time::Duration;
+
+async fn std_out_hello() {
+    let mut interval = tokio::time::interval(Duration::from_secs(5));
+
+    loop {
+        interval.tick().await;
+        println!("Hello, World!");
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    tokio::spawn(std_out_hello());
+
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
+
+    // run it
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await;
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
+}
